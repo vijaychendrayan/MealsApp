8b4808bcb26ce9a7bac3e2b2ae8e2ee239724a29
Vijay Chendrayan Chandrasekar <vijaychendrayan@gmail.com>
2023-05-04 13:01:09 +0530

    Second

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import CategoriesScreen from './screen/CategoriesScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <CategoriesScreen />
+    </>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
